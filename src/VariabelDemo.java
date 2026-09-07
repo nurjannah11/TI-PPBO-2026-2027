0aa6b18a1a8e1d43c809cf3a516aba39ef3c9658
nurjannah11 <tg8tzswht6@privaterelay.appleid.com>
2026-09-07 11:19:42 +0700

    B.1 - deklarasi variabel dan tipe data primitif

---
+++ src/VariabelDemo.java
@@ -0,0 +1,16 @@
+public class VariabelDemo {
+    public static void main(String[] args) {
+
+        String nama = "NurJannah";
+        int umur = 19;
+        double tinggi = 160;
+        char golDarah = 'O';
+        boolean mahasiswaAktif = true;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Golongan Darah: " + golDarah);
+        System.out.println("Aktif: " + mahasiswaAktif);
+    }
+}
