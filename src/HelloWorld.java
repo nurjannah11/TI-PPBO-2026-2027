944fc44e5e786b9f50b4607cc5bcfa3e37ca439b
nurjannah11 <tg8tzswht6@privaterelay.appleid.com>
2026-09-07 11:05:27 +0700

    A.2 - perbedaan println() dan print()

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {  // ← HARUS sama dengan nama file!
     public static void main(String[] args) {
-        System.out.println("Halo, nama saya jannah!");
+        System.out.println("Halo,jannah!");
+
+        System.out.print("Nama: ");
+        System.out.print("jannah");
+        System.out.println();
+        System.out.println("ini baris baru");
     }
 }
--- src/VariabelDemo.java
+++ src/VariabelDemo.java
@@ -1,2 +1,11 @@
-public class VariabelDemo {
+public class VariabelDemo {  // ← HARUS sama dengan nama file!
+    public static void main(String[] args) {
+        System.out.println("Halo,jannah!");
+
+        System.out.print("Nama: ");
+        System.out.print("jannah");
+        System.out.println();
+        System.out.println("ini baris baru");
+    }
 }
+
