4234b96a6773d8ee7186f4a80819c415c374298e
nurjannah11 <tg8tzswht6@privaterelay.appleid.com>
2026-09-07 20:01:51 +0700

    latihan 1

---
+++ src/BiodataSaya.java
@@ -0,0 +1,10 @@
+public class BiodataSaya  {
+    public static void main(String[] args) {
+
+        System.out.print("nama: Nurjannah ");
+
+        System.out.println("NIM: 202553010108");
+
+        System.out.print("Program Studi: Teknik Informatika");
+    }
+}
